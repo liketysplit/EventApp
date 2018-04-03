5061a1b2e45744bc8faa817cd5bc9b37314bec90
liketysplit <rjboles2@gmail.com>
2018-04-02 23:26:54 -0400

    database fix

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI:'mongodb://<minecraft-prod>:<plan>@ds153845.mlab.com:53845/minecraft-prod'}
-}else{
-    module.exports = {mongoURI:'mongodb://localhost/mcideas-dev'}
-}
+    module.exports = {mongoURI: 'mongodb://minecraft:plan@ds153845.mlab.com:53845/minecraft-prod'}
+  } else {
+    module.exports = {mongoURI: 'mongodb://localhost/mcideas-dev'}
+  }
